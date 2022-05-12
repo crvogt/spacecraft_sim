70579c8ffa46ad91cc96a5009a569733063d2300
crvogt <crvogt26@gmail.com>
2022-05-12 11:45:07 -0700

    Added strafe thrusters and manual control mixing

--- sc_control/src/sc_manual_thrusterop.py
+++ sc_control/src/sc_manual_thrusterop.py
@@ -10,19 +10,22 @@ class ThrustTeleop:
 
         self.gain_dict = dict()
         # Forward/Back 0 1 2 3
-        self.gain_dict[1] = [1.0, 1.0, -1.0, -1.0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.gain_dict[1] = [1.0, 1.0, -1.0, -1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         # up 4 5
-        self.gain_dict[5] = [0, 0, 0, 0, -0.5, -0.5, 0, 0, 0, 0, 0, 0]
+        self.gain_dict[5] = [0, 0, 0, 0, -0.5, -0.5, 0, 0, 0, 0, 0, 0, 0, 0]
         # down 6 7
-        self.gain_dict[2] = [0, 0, 0, 0, 0, 0, -0.5, -0.5, 0, 0, 0, 0]
+        self.gain_dict[2] = [0, 0, 0, 0, 0, 0, -0.5, -0.5, 0, 0, 0, 0, 0, 0]
         # roll 4 5 6 7
-        self.gain_dict[3] = [0, 0, 0, 0, -1.0, 1.0, 1.0, -1.0, 0, 0, 0, 0]
+        self.gain_dict[3] = [0, 0, 0, 0, -1.0, 1.0, 1.0, -1.0, 0, 0, 0, 0, 0, 0]
         # pitch 0 1 2 3
-        self.gain_dict[4] = [1.0, -1.0, -1.0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.gain_dict[4] = [1.0, -1.0, -1.0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         # yaw 8 9 10 11
-        self.gain_dict[0] = [0, 0, 0, 0, 0, 0, 0, 0, -1.0, 1.0, 1.0, -1.0]
+        self.gain_dict[0] = [0, 0, 0, 0, 0, 0, 0, 0, -1.0, 1.0, 1.0, -1.0, 0, 0]
+        # strafe rb = 6th
+        # 4 5 6 7
+        self.strafe_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0, -1.0]
 
-        self.num_thrusters = 12
+        self.num_thrusters = 14
 
         self.joy2thrust = [rospy.Publisher('/%s/thrusters/thruster_%d/'%(namespace, 0), Float32, queue_size=1),
                            rospy.Publisher('/%s/thrusters/thruster_%d/'%(namespace, 1), Float32, queue_size=1),
@@ -35,7 +38,9 @@ class ThrustTeleop:
                            rospy.Publisher('/%s/thrusters/thruster_%d/'%(namespace, 8), Float32, queue_size=1),
                            rospy.Publisher('/%s/thrusters/thruster_%d/'%(namespace, 9), Float32, queue_size=1),
                            rospy.Publisher('/%s/thrusters/thruster_%d/'%(namespace, 10), Float32, queue_size=1),
-                           rospy.Publisher('/%s/thrusters/thruster_%d/'%(namespace, 11), Float32, queue_size=1)]
+                           rospy.Publisher('/%s/thrusters/thruster_%d/'%(namespace, 11), Float32, queue_size=1),
+                           rospy.Publisher('/%s/thrusters/thruster_%d/'%(namespace, 12), Float32, queue_size=1),
+                           rospy.Publisher('/%s/thrusters/thruster_%d/'%(namespace, 13), Float32, queue_size=1)]
 
         self.joy_sub = rospy.Subscriber('joy', Joy, self.joy_callback)
         
@@ -46,9 +51,18 @@ class ThrustTeleop:
     def joy_callback(self, joy):
         msg = Float32()
         aa_dict = dict()
+        bb_dict = dict()
 
         for aa, ii in zip(joy.axes, range(len(joy.axes))):
             aa_dict[ii] = aa
+        for bb, ii, in zip(joy.buttons, range(len(joy.buttons))):
+            bb_dict[ii] = bb
+       
+        # On button press, load strafe gains
+        if bb_dict[5] == 1:
+            self.gain_dict[0] = self.strafe_list
+        else:
+            self.gain_dict[0] = [0, 0, 0, 0, 0, 0, 0, 0, -1.0, 1.0, 1.0, -1.0, 0, 0]
 
         key_list = list(self.gain_dict.keys())
         msg_values = [0] * self.num_thrusters
