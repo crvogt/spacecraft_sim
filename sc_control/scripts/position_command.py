56314895815c2726d299e04c28e95eee6562e6e8
crvogt <crvogt26@gmail.com>
2022-05-19 18:20:55 -0700

    Added command node, beginning debugging for control node

---
+++ sc_control/scripts/position_command.py
@@ -0,0 +1,37 @@
+import numpy as np
+import rospy
+
+from geometry_msgs.msg import PoseStamped
+
+class PositionCommandNode:
+    def __init__(self):
+        print("PositionCommandNode: initializing node")
+
+        self.cmd_val = PoseStamped()
+        self.cmd_val.pose.position.x = 1.0
+        self.cmd_val.pose.position.y = 0.0
+        self.cmd_val.pose.position.z = 2.0
+        self.cmd_val.pose.orientation.x = 0.0
+        self.cmd_val.pose.orientation.y = 0.0
+        self.cmd_val.pose.orientation.z = 0.0
+        self.cmd_val.pose.orientation.w = 1.0
+
+        self.cmd_pub = rospy.Publisher('cmd_pose', PoseStamped, queue_size=1)
+
+        rate = rospy.Rate(50)
+        while not rospy.is_shutdown():
+            self.cmd_val.header.stamp = rospy.Time.now()
+            self.cmd_pub.publish(self.cmd_val)
+            rate.sleep()
+
+if __name__=="__main__":
+    print('Starting position_command.py')
+    rospy.init_node('position_command')
+
+    try:
+        node = PositionCommandNode()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        print('Caught an exception')
+    print('exciting')
+        
--- sc_control/scripts/position_control.py
+++ sc_control/scripts/position_control.py
@@ -5,6 +5,7 @@ import tf.transformations as trans
 from rospy.numpy_msg import numpy_msg
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import PoseStamped 
+from std_msgs.msg import Float32
 from PID import PIDControllerBase 
 
 from gazebo_msgs.msg import ModelStates
@@ -19,23 +20,36 @@ class PositionControllerNode:
         self.getPosCmd = PoseStamped() 
         self.poseVal = PoseStamped()
         self.t_val = 0
+        self.num_thrusters = 14
+        self.msg_vals = np.asarray([0] * self.num_thrusters)
         
         # Set PID values
         # Simple split
         # R, P, Y, x, y, z
-        self.p_vals = np.array([0, 0, 0, 0, 0, 1])
-        self.i_vals = np.array([0, 0, 0, 0, 0, 1])
-        self.d_vals = np.array([0, 0, 0, 0, 0, 1])
-        self.sat_vals = np.array([0, 0, 0, 0, 0, 1])
+        self.p_vals = np.array([0, 0, 0, 1, 0, 0])
+        self.i_vals = np.array([0, 0, 0, 0, 0, 0])
+        self.d_vals = np.array([0, 0, 0, 0, 0, 0])
+        self.sat_vals = np.array([1, 1, 1, 1, 1, 1])
         self.pid_list = [0] * 6 
         self.set_pid_vals()
+        self.pid_out = [0] * 6
 
+        # Putting gain dicts here...
+        self.gain_dict = dict()
+        self.gain_dict[0] = np.asarray([0, 0, 0, 0, -1.0, 1.0, 1.0, -1.0, 0, 0, 0, 0, 0, 0])
+        self.gain_dict[1] = np.asarray([1.0, -1.0, -1.0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.gain_dict[2] = np.asarray([0, 0, 0, 0, 0, 0, 0, 0, -1.0, 1.0, 1.0, -1.0, 0, 0])
+        self.gain_dict[3] = np.asarray([1.0, 1.0, -1.0, -1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.gain_dict[4] = np.asarray([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0, -1.0])
+        self.gain_dict[5] = np.asarray([0, 0, 0, 0, -0.5, -0.5, -0.5, -0.5, 0, 0, 0, 0, 0, 0])
+        
         # Set flags
         self.initialized = False
         # Set this to false till we can determine 3D pose
         self.use_imu = False
 
         # ROS
+        self.thrust_msg = Float32()
         self.sub_cmd_pose = rospy.Subscriber('cmd_pose', PoseStamped, self.cmd_pose_callback)
         self.position_sub = rospy.Subscriber('polysat/imu', Imu, self.odometry_callback)
         self.model_state_sub = rospy.Subscriber('gazebo/model_states', ModelStates, self.state_callback)
@@ -64,6 +78,7 @@ class PositionControllerNode:
         self.poseVal
 
         # Angular error
+        # could get the quat diff, then get the angle from the quaternion...
         cmd_angles = trans.euler_from_quaternion(self.getPosCmd.pose.quaternion)
         cur_angles = trans.euler_from_quaternion(self.poseVal.pose.quaternion)
         Rol_err = cmd_angles[0] - cur_angles[0] 
@@ -79,8 +94,21 @@ class PositionControllerNode:
 
         error_list = [Rol_err, Pit_err, Yaw_err,
                       x_err, y_err, z_err]
-        # Send values to regulator
 
+        # Send values to regulator
+        for ii, pid_reg in enumerate(self.pid_list):
+            self.pid_out[ii] = pid_reg.regulate(error_list[ii], self.t_val)
+
+        self.publish_vals()
+
+    def publish_vals(self):
+        # multiply gains 
+        for ii, pid_val in enumerate(self.pid_out):
+            self.msg_vals += self.gain_dict[ii] * pid_val
+
+        for ii, thruster in enumerate(self.thrust_list):
+            self.thrust_msg.data = self.msg_vals[ii]
+            thruster.publish(self.thrust_msg)
 
     def thruster_pubs(self):
         self.thrust_list = [rospy.Publisher('/%s/thrusters/thruster_%d/'%(namespace, 0), Float32, queue_size=1),
@@ -100,11 +128,10 @@ class PositionControllerNode:
 
     def set_pid_vals(self):
         for ii in range(6):
-            self.pid_list[ii] = PIDRegulator(self.p_vals[ii],
+            self.pid_list[ii] = PIDControllerBase(self.p_vals[ii],
                                              self.i_vals[ii],
                                              self.d_vals[ii],
                                              self.sat_vals[ii])
-
 
 if __name__=="__main__":
     print('Starting PositionControl.py')
