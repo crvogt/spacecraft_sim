ab8a514e13ed5fe587fbc04fa19fea9645fc976e
crvogt <crvogt26@gmail.com>
2022-07-12 14:52:20 -0700

    Added moveit files and configurations. Need to remove/find empty joint

--- sc_control/scripts/position_control.py
+++ sc_control/scripts/position_control.py
@@ -27,8 +27,8 @@ class PositionControllerNode:
         # Set PID values
         # Simple split
         # R, P, Y, x, y, z
-        self.p_vals = np.array([5.0, 5.5, 5.0, 3.0, 3.0, 3.0])
-        self.i_vals = np.array([3.0, 3.0, 3.0, 1.5, 1.5, 1.5])
+        self.p_vals = np.array([5.0, 4.5, 5.0, 3.0, 3.0, 3.0])
+        self.i_vals = np.array([3.0, 2.5, 3.0, 1.5, 1.5, 1.5])
         self.d_vals = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
         self.sat_vals = np.array([500, 500, 500, 10, 10, 10])
         self.pid_list = [0] * 6 
