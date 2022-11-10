3df2200dc712b87a1eb0ad00f90c3fd407a62173
crvogt <crvogt26@gmail.com>
2022-11-10 13:52:13 -0800

    Added motor colors

--- sc_utils/bash_scripts/add_plugin_lines.py
+++ sc_utils/bash_scripts/add_plugin_lines.py
@@ -8,6 +8,12 @@ noz_thrusters = 14
 noz_count = 0
 sat_viz = 1
 sat_count = 0
+dyna_motors = 8
+dyna_count = 0
+link_count = 0
+
+dyna_locs = list()
+link_locs = list()
 
 counter = 0
 ii = 0
@@ -24,6 +30,10 @@ while True:
     thrust_material = '\t\t\t\t\t<material>\n\t\t\t\t\t\t<ambient>0.1 0.1 0.1 1</ambient>\n\t\t\t\t\t\t<diffuse>0.1 0.1 0.2 1</diffuse>\n\t\t\t\t\t\t<specular>0 0 0 0</specular>\n\t\t\t\t\t\t<emissive>0 0 0 1</emissive>\n\t\t\t\t\t</material>\n'
     # Sat body color
     sat_material = '\t\t\t\t\t<material>\n\t\t\t\t\t\t<ambient>1.0 0.84 0 1</ambient>\n\t\t\t\t\t\t<diffuse>1.0 0.8 0 1</diffuse>\n\t\t\t\t\t\t<specular>1.0 0.8 0 1</specular>\n\t\t\t\t\t\t<emissive>0 0 0 1</emissive>\n\t\t\t\t\t</material>\n'
+    # Dynamixel motor color
+    dyna_material = '\t\t\t\t\t<material>\n\t\t\t\t\t\t<ambient>0.1 0.1 0.1 1</ambient>\n\t\t\t\t\t\t<diffuse>0.1 0.1 0.2 1</diffuse>\n\t\t\t\t\t\t<specular>0 0 0 0</specular>\n\t\t\t\t\t\t<emissive>0 0 0 1</emissive>\n\t\t\t\t\t</material>\n'
+    # Arm link color
+    link_material = '\t\t\t\t\t<material>\n\t\t\t\t\t\t<ambient>1 1 1 1</ambient>\n\t\t\t\t\t\t<diffuse>0.5 0.5 0.5 1</diffuse>\n\t\t\t\t\t\t<specular>0.1 0.1 0.1 0.1</specular>\n\t\t\t\t\t\t<emissive>0 0 0 1</emissive>\n\t\t\t\t\t</material>\n'
 
     # Go through each line and check for the thrust plume visual
     while ii < line_num:
@@ -57,6 +67,32 @@ while True:
             ii = 0
             line_num = len(world_lines)
 
+        # Add motor colors
+        if 'perseus' in world_lines[ii] and 'visual' in world_lines[ii] and 'Dynamixel' in world_lines[ii] and dyna_count < dyna_motors:
+            if world_lines[ii] in dyna_locs:
+                pass
+            else:
+                dyna_locs.append(world_lines[ii])
+                print("On motor {}".format(dyna_count))
+                id_location = ii + 1
+                world_lines.insert(id_location, dyna_material)
+                dyna_count += 1
+                ii = 0
+                line_num = len(world_lines)
+
+        # Add link colors
+        if 'perseus' in world_lines[ii] and 'visual' in world_lines[ii]:
+            if 'Dynamixel' not in world_lines[ii]:
+                if world_lines[ii] in link_locs:
+                    pass
+                else:
+                    link_locs.append(world_lines[ii])
+                    print("On link {}".format(link_count))
+                    id_location = ii + 1
+                    world_lines.insert(id_location, link_material)
+                    link_count += 1
+                    ii = 0
+                    line_num = len(world_lines)
         ii += 1
 
     if ii == line_num:
